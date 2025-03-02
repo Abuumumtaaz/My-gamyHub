54288d5b6cc63b335835193d22a5f984618c9030
Abuumumtaaz <ahmedqayr22@gmail.com>
2025-03-02 12:03:16 +0000

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button, HStack } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div>
+      <p>bismillah</p>
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+        <Button>Button</Button>
+      </HStack>
+    </div>
+  );
+}
+
+export default App;
